e66f062f946df394a46f95f1f245e182cbc23792
jangjisu <jjs62611615@gmail.com>
2025-07-31 22:53:15 +0900

    [feat] Keyword 클래스에 equals 및 hashCode 메서드 추가 및 생성자 개선

--- src/main/java/com/app/keywordwatcher/domain/keyword/Keyword.java
+++ src/main/java/com/app/keywordwatcher/domain/keyword/Keyword.java
@@ -3,16 +3,26 @@ package com.app.keywordwatcher.domain.keyword;
 import com.app.keywordwatcher.domain.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@EqualsAndHashCode(callSuper = false)
 public class Keyword extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String keyText;
+
+    private Keyword(String keyText) {
+        this.keyText = keyText;
+    }
+
+    public static Keyword create(String keyText) {
+        return new Keyword(keyText);
+    }
 }
