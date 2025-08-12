a7f32fe192067578205cf4d26c9ea4676ef32e25
jangjisu <jjs62611615@gmail.com>
2025-08-12 23:28:25 +0900

    feat: enhance authentication requests with validation and error handling

--- src/main/java/com/app/keywordwatcher/web/controller/user/UserController.java
+++ src/main/java/com/app/keywordwatcher/web/controller/user/UserController.java
@@ -1,7 +1,7 @@
 package com.app.keywordwatcher.web.controller.user;
 
 import com.app.keywordwatcher.web.controller.ApiResponse;
-import com.app.keywordwatcher.web.dto.UserResponse;
+import com.app.keywordwatcher.web.controller.user.response.UserResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
