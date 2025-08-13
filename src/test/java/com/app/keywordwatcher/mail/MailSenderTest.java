836af0772123ec431b83283428c59a67e8d7e140
jangjisu <jjs62611615@gmail.com>
2025-08-13 22:57:02 +0900

    feat: 메일 폼 만드는 부분과 메일 보내는 부분 추가

---
+++ src/test/java/com/app/keywordwatcher/mail/MailSenderTest.java
@@ -0,0 +1,44 @@
+package com.app.keywordwatcher.mail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+
+class MailSenderTest {
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    private MailSender mailSender;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mailSender = new MailSender(javaMailSender);
+    }
+
+    @DisplayName("메일을 전송한다.")
+    @Test
+    void sendMail() {
+        // given
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("test@example.com");
+        message.setSubject("테스트 제목");
+        message.setText("테스트 내용");
+
+        doNothing().when(javaMailSender).send(message);
+
+        // when
+        mailSender.sendMail(message);
+
+        // then
+        verify(javaMailSender).send(message);
+    }
+}
