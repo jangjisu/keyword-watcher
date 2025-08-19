72892ff240f9c1ffdd25f9b1f3ceda58ca041eb3
jangjisu <jjs62611615@gmail.com>
2025-08-19 21:31:43 +0900

    feat: Site 와 Keyword 매핑 테이블 siteKeyword 삭제

--- src/main/java/com/app/keywordwatcher/domain/sitekeyword/SiteKeywordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.keywordwatcher.domain.sitekeyword;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface SiteKeywordRepository extends JpaRepository<SiteKeyword, Long> {
-    List<SiteKeyword> findBySiteId(Long siteId);
-}
