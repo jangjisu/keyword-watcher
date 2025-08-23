02ad461517fdfd3e98c45fc6440b943e6bd320da
jangjisu <jjs62611615@gmail.com>
2025-08-23 12:51:30 +0900

    feat: keyword Service 및 Repository 추

--- src/main/java/com/app/keywordwatcher/domain/keyword/KeywordRepository.java
+++ src/main/java/com/app/keywordwatcher/domain/keyword/KeywordRepository.java
@@ -2,6 +2,8 @@ package com.app.keywordwatcher.domain.keyword;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+import java.util.Optional;
 
+public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+    Optional<Keyword> findByKeyText(String keyword);
 }
---
+++ src/main/java/com/app/keywordwatcher/web/service/keyword/KeywordService.java
@@ -0,0 +1,36 @@
+package com.app.keywordwatcher.web.service.keyword;
+
+import com.app.keywordwatcher.domain.keyword.Keyword;
+import com.app.keywordwatcher.domain.keyword.KeywordRepository;
+import com.app.keywordwatcher.web.controller.keyword.request.KeywordRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class KeywordService {
+    private final KeywordRepository keywordRepo;
+
+    /**
+     * 키워드 생성 (시스템 전체용)
+     */
+    @Transactional
+    public Keyword createIfNonExistKeyword(KeywordRequest keywordRequest) {
+        String norm = normalize(keywordRequest.getKeyText());
+
+        return keywordRepo.findByKeyText(norm)
+                .orElseGet(() -> keywordRepo.save(keywordRequest.toKeyword()));
+    }
+
+    public Keyword getKeyword(KeywordRequest keywordRequest) {
+        String norm = normalize(keywordRequest.getKeyText());
+        return keywordRepo.findByKeyText(norm)
+                .orElseThrow(() -> new IllegalArgumentException("키워드를 찾을 수 없습니다."));
+    }
+
+    private String normalize(String keyText) {
+        return keyText.trim().toLowerCase();
+    }
+}
